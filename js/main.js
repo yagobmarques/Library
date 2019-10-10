b40366c0567c425f7486739ac3749033bc26d841
Yago Beserra Marques <yagomarques@A309-14L.imd.local>
2019-10-10 18:14:14 -0300

    Tá ficando bonito, começando o processo de cadastrar um livro

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function mostrarCadastro(){
+	document.getElementById("cadastro").style.display = 'block'
+}
+function fecharCadastro(){
+	document.getElementById("cadastro").style.display = 'none'
+}
