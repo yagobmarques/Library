12add244d62b5e9a1623eb6e1b63038538db7716
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-10-11 11:47:46 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-function mostrarCadastro(){
-	document.getElementById("cadastro").style.display = 'block'
+var books = [];
+function addBook(){
+	book = {title: "Title",  author: "Author", pages: 0,status: "none"};
+	book.title = $("#title").val();
+	book.author = $("#author").val();
+	book.pages = $("#pages").val();
+	book.status = $("#status").val();
+	books.push(book);
+	closeModal("#modalCadastro");
 }
-function fecharCadastro(){
-	document.getElementById("cadastro").style.display = 'none'
+function closeModal(id){
+	$(id).modal('hide');
+}
+function showBooks(){
+	clearBooks();
+	for (i = 0; i<books.length; i++){
+		title = "<td>" +books[i].title + "</td>";
+		author = "<td>" +books[i].author + "</td>";
+		pages = "<td>" +books[i].pages + "</td>";
+		status = "<td>" +books[i].status + "</td>";
+		button = "<td>" + "<button type=\"button\" class=\"btn btn-dark\" onclick=\"remove()\" value=\""+books[i].title+"\"> Remove</button>"; 
+		$("#tablebody").append("<tr>"+title+author+pages+status+button+"</tr>");
+	}
+}
+function clearBooks(){
+	$("#tablebody").empty();
 }
